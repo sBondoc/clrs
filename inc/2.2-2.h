4eff5864bd85cd7e50c003e088036ba09688a79e
sBondoc <sabondoc@uci.edu>
2022-04-10 06:42:29 +0100

    Skeleton code for selection sort.

---
+++ inc/2.2-2.h
@@ -0,0 +1,10 @@
+/* "2.2-2.h" - Prototypes for 2.2-2 functions. */
+#ifndef TWO_POINT_ONE_DASH_TWO_H
+#define TWO_POINT_ONE_DASH_TWO_H
+/* Swap two array elements. */
+void swap(int *arr, int i, int j);
+/* Get index of smallest array element in range. */
+int min(int *arr, int i, int j);
+/* Basic selection sort. */
+void sort_selection(int *arr, int size);
+#endif
---
+++ src/2.2-2.c
@@ -0,0 +1,17 @@
+/* "2.2-2.h" - Implementation for 2.2-2 functions. */
+#include "../inc/2.2-2.h"
+/* Swap two array elements. */
+void swap(int *arr, int i, int j)
+{
+	// code
+}
+/* Get index of smallest array element in range. */
+int min(int *arr, int i, int j)
+{
+	// code
+}
+/* Basic selection sort. */
+void sort_selection(int *arr, int size)
+{
+	// code
+}
