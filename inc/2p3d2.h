7037eab985bfa6e725ef8afad27e06b091f789c0
sBondoc <sabondoc@uci.edu>
2022-04-10 20:31:15 +0100

    Skeleton code for 2.3-2 functions.

---
+++ inc/2p3d2.h
@@ -0,0 +1,8 @@
+/* "2p3d2.h" - Prototypes for 2.3-2 functions. */
+#ifndef TWO_POINT_THREE_DASH_TWO_H
+#define TWO_POINT_THREE_DASH_TWO_H
+/* Recursive merge sort no sentinel merge step. */
+void sort_merge_no_sentinel_merge(int *arr, int p, int q, int r);
+/* Recursive merge sort no sentinel. */
+void sort_merge_no_sentinel(int *arr, int p, int r);
+#endif
---
+++ src/2p3d2.c
@@ -0,0 +1,13 @@
+/* "2p3d2.c" - Implementation for 2.3-2 functions. */
+#include <string.h>
+#include "../inc/2p3d2.h"
+/* Recursive merge sort no sentinel merge step. */
+void sort_merge_no_sentinel_merge(int *arr, int p, int q, int r)
+{
+	// code
+}
+/* Recursive merge sort no sentinel. */
+void sort_merge_no_sentinel(int *arr, int p, int r)
+{
+	// code
+}
