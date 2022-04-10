782c1fea3b4980b03dbcc066ad3f1f37fda8723b
sBondoc <sabondoc@uci.edu>
2022-04-10 17:46:43 +0100

    Skeleton code for 2.3 functions.

---
+++ inc/2p3.h
@@ -0,0 +1,8 @@
+/* "2p3.h" - Prototypes for 2.3 functions. */
+#ifndef TWO_POINT_THREE_H
+#define TWO_POINT_THREE_H
+/* Recursive merge sort merge step. */
+void sort_merge_merge(int *arr, int p, int q, int r);
+/* Recursive merge sort. */
+void sort_merge(int *arr, int p, int r);
+#endif
---
+++ src/2p3.c
@@ -0,0 +1,12 @@
+/* "2p3.c" - Implementation for 2.3 functions. */
+#include "../inc/2p3.h"
+/* Recursive merge sort merge step. */
+void sort_merge_merge(int *arr, int p, int q, int r)
+{
+	// code
+}
+/* Recursive merge sort. */
+void sort_merge(int *arr, int p, int r)
+{
+	// code
+}
