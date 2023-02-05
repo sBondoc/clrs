6a910e827608d305cef2c236006071cc90494687
sBondoc <sabondoc@uci.edu>
2023-02-05 03:47:31 -0800

    Subarray struct modularization.

--- inc/4p1.h
+++ inc/4p1.h
@@ -1,13 +1,7 @@
 /* "4p1.h" - Prototypes for 4.1 functions. */
 #ifndef FOUR_POINT_ONE_H
 #define FOUR_POINT_ONE_H
-/* Subarray struct */
-struct Subarray
-{
-	int p;
-	int r;
-	int sum;
-};
+#include "subarray.h"
 /* Find max subarray crossing midpoint. */
 struct Subarray max_subarray_crossing(int *arr, int p, int q, int r);
 /* Find max subarray. */
---
+++ inc/subarray.h
@@ -0,0 +1,10 @@
+/* "subarray.h" - Definition for subarray struct. */
+#ifndef SUBARRAY_H
+#define SUBARRAY_H
+struct Subarray
+{
+	int p;
+	int r;
+	int sum;
+};
+#endif
